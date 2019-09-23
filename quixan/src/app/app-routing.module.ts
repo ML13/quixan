57fbc7ca9613831bf9b8570a6dec98c5720b90a3
ML <luhovy@kros.sk>
2019-09-23 14:08:26 +0200

    article service

--- quixan/src/app/app-routing.module.ts
+++ quixan/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'authors', component: NavBarComponent },
+  { path: '', component: NavBarComponent },
 ];
 
 @NgModule({
