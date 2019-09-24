0743bef9e6fc2f0fd90ac8bb33fc6b35e2e7e645
JakubCelovsky <celovsky.jakubmail.com>
2019-09-24 13:52:21 +0200

    pred zmenou store

--- quixan/src/app/nav/articles/articles.component.ts
+++ quixan/src/app/nav/articles/articles.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from 'src/app/core/article.model';
 import { ArticleService } from 'src/app/core/article.service';
 import { AuthorSelectorService } from 'src/app/store/state/selectors';
-import { switchMap, tap } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { switchMap, tap, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { Author } from 'src/app/core/author.model';
 
 @Component({
   selector: 'app-articles',
@@ -14,6 +15,7 @@ import { Observable } from 'rxjs';
 export class ArticlesComponent implements OnInit {
 
   articles$: Observable<Article[]>;
+  isReady$: Observable<boolean>;
 
   constructor(
     private articleService: ArticleService,
@@ -22,16 +24,36 @@ export class ArticlesComponent implements OnInit {
 
   ngOnInit() {
     this.displayArticles();
+    this.jahoda();
+    // this.articles$ = this.displayArticles;
   }
+
+  // private get displayArticles(): Observable<any> {
+  //   this.articles$ = null;
+  //   this.isReady = false;
+  //   return this.authorSelectorService.CurrentAuthor$.pipe(
+  //     switchMap(
+  //       author => author === null ? this.articleService.getArticles() : this.articleService.getArticlesByAuthor(author.id),
+  //     ),
+  //     () => this.isReady = true,
+  //     tap(
+  //       author => console.log(author)
+  //     )
+  //   );
+  // }
 
   displayArticles(): void {
     this.articles$ = this.authorSelectorService.CurrentAuthor$.pipe(
       switchMap(
         author => author === null ? this.articleService.getArticles() : this.articleService.getArticlesByAuthor(author.id),
       ),
-      tap(
-        author => console.log(author)
-      )
     );
+    // this.isReady = true;
+
+  }
+
+  jahoda(): void {
+    this.isReady$ = of(false);
+    this.articles$.pipe(map(() => this.isReady$ = of(true)));
   }
 }
